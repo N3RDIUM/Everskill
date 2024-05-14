2263d3a4370433d711e17fd360edc03c36d701fc
n3rdium <n3rdium@gmail.com>
2024-05-14 07:51:14 +0530

    Update course code

--- src/API/api/course.py
+++ src/API/api/course.py
@@ -1,22 +1,10 @@
 # Imports
-import re
 import json
 import bleach
 import requests
-from html_sanitizer import Sanitizer
-sanitizer = Sanitizer({
-    "tags": {
-        "a", "h1", "h2", "h3", "strong", "em", "p", "ul", "ol",
-        "li", "br", "sub", "sup", "hr", "div", "pre"
-    },
-    "attributes": {"a": ("href", "name", "target", "title", "id", "rel"), "div": ("class", "data-quizid")},
-    "empty": {"hr", "a", "br"},
-    "separate": {"a", "p", "li"},
-    "whitespace": {"br"},
-    "is_mergeable": lambda e1, e2: True,
-})
 
 # Course class
+# TODO! Use the html sanitizer!
 class Course:
     def __init__(self, url: str) -> None:
         self.url = url
@@ -25,15 +13,10 @@ class Course:
     def render_html(self, page: str = 'home') -> list:
         page = self.details['pages'][page]
         source = requests.get(page['html']).text
-        ret = sanitizer.sanitize(source.strip()).strip()
-        ret = bleach.linkify(ret)
-        
-        # Get all instances of comments in this html
-        comments = re.findall('<!--.*?-->', ret)
-        for comment in comments:
-            c = comment.replace('<!--', '').replace('-->', '').strip()
-            if c.startswith('QUIZ_EMBED'):
-                quizid = c.split(':')[1].strip()
-                ret = ret.replace(comment, f'<div id="quiz-embed" data-quizid="{quizid}"></div>')
+        ret = bleach.linkify(source)
         
         return page, ret
+    
+c = Course("https://raw.githubusercontent.com/N3RDIUM/everskill-course-template/main/course.json")
+r = c.render_html()
+print(r)
